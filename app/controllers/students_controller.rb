4c5f186e5a883c0aa77b0eb8db779c2fe4de7216
Aubrey Neimeier <neimeier.1@osu.edu>
2018-04-25 17:20:17 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,9 @@
 class StudentsController < ApplicationController
 # At a high level, you need to build a form to create a new student, have the form
-#  redirect back to the new view template, and print out the form params to the 
-#  screen. Below are a few items to keep in mind:
+#  redirect back to the new view template, and print out the form params to the
+#  screen. Below are a few items to keep in mind
+# -Draw a new and create route for the students resource
+# -Print out the student value to the view template page
   def index
     @students = Student.all
   end
